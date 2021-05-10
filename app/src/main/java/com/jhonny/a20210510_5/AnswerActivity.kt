b8fca2bbc8059cbfaf2d91e6bfd314c08cbdb12d
jhonnywinarta <jhonnywinarta@gmail.com>
2021-05-10 15:57:44 +0800

    Initial commit

---
+++ app/src/main/java/com/jhonny/a20210510_5/AnswerActivity.kt
@@ -0,0 +1,31 @@
+package com.jhonny.a20210510_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_answer.*
+
+class AnswerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answer)
+
+        val intent = getIntent()
+        val yourans:Int = intent.getIntExtra("answer",0)
+        val rightans:Int = intent.getIntExtra("correct",0)
+
+        yourAnswer.text = yourans.toString()
+        rightAnswer.text = rightans.toString()
+
+        if(yourans!=rightans){
+            textComment.text = "Your answer is wrong!"
+        }
+        else textComment.text = "Your answer is correct!"
+
+        btnReturn.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v:View?){
+                finish()
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/jhonny/a20210510_5/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.jhonny.a20210510_5
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        refresh()
+
+        btnRefresh.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(v: View?) {
+            refresh()
+            }
+        })
+        btnAns.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(v: View?) {
+                intent=Intent(this@MainActivity,AnswerActivity::class.java).apply {
+                    putExtra("answer",answer.text.toString().toInt())
+                    putExtra("correct",x.text.toString().toInt()+y.text.toString().toInt())
+                }
+                startActivity(intent)
+            }
+
+        })
+    }
+    fun refresh(){
+        val ranx=(1..100).random()
+        val rany=(1..100).random()
+
+        x.text=ranx.toString()
+        y.text=rany.toString()
+    }
+    
+}
